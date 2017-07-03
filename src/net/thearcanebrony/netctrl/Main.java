036c6b6dfdfc6717a13ec72cfa003338b6f80c73
TheArcaneBrony <myrainbowdash949@gmail.com>
2017-07-03 22:39:47 +0200

    <@&324265687378034689> Y'all need to start doing stuff...
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- src/net/thearcanebrony/netctrl/Main.java
+++ src/net/thearcanebrony/netctrl/Main.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
